13dbe401373233d569ef483b2630baacacd7111b
qushe8r <qushe8r@gmail.com>
2023-10-08 21:25:54 +0900

    [feat] UserController UserControllerTest init
    - UserController
    - UserControllerTest

---
+++ src/main/java/com/wanted/preonboardingbackend/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.wanted.preonboardingbackend.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
---
+++ src/test/java/com/wanted/preonboardingbackend/user/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.wanted.preonboardingbackend.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void register() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
