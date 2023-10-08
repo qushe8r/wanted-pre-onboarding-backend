94fa299919883906ee026508f92b92c1835f84ff
qushe8r <qushe8r@gmail.com>
2023-10-08 22:14:05 +0900

    [feat] JobPostingController JobPostingControllerTest init
    - JobPostingController
    - JobPostingControllerTest

---
+++ src/main/java/com/wanted/preonboardingbackend/jobposting/controller/JobPostingController.java
@@ -0,0 +1,11 @@
+package com.wanted.preonboardingbackend.jobposting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/job-postings")
+public class JobPostingController {
+}
---
+++ src/test/java/com/wanted/preonboardingbackend/jobposting/controller/JobPostingControllerTest.java
@@ -0,0 +1,24 @@
+package com.wanted.preonboardingbackend.jobposting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(JobPostingController.class)
+class JobPostingControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void write() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
